5dcce76dee12ce0c7875707e60a990ef82d36549
hsisco <hysensisco@gmail.com>
2020-03-15 20:29:01 -0700

    Update postRouter.js

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,3 +1,14 @@
-const express = require('express')
-const db = require('../data/db')
-const router = express.Router()
+const express = require('express');
+const db = require('../data/db');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+  }
+  db.insert(req.body)
+  .then(post => res.status(201).json(req.body))
+  .catch(err => res.status(500).json({ errorMessage: "There was an error while saving the post to the database", success: false, err }))
+});
